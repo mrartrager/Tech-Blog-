8d69aac7171b66418b474e37ee27d26a82185520
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-12-02 17:27:03 -0600

    added more to server.js

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const exphbs = require('express-handlebars');
 const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
