512dc125795a17ee598d12569a15026d7d5c9c94
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-12-03 15:16:41 -0600

    models now complete

--- models/index.js
+++ models/index.js
@@ -4,12 +4,24 @@ const Comment = require('./Comment');
 
 
 User.hasMany(Post, {
-
+    foreignKey: 'user_id'
 });
 
-Post.belongsTo(User, {
-    
+Post.hasMany(Comment, {
+    foreignKey: 'post_id'
 })
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey:'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey:'post_id'
+});
 // User.(,{
 
 // })
