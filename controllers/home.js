ccb1af14d0d57a9fbb442099fff67bf05280b090
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-12-03 15:30:37 -0600

    logout route in home completed_other routes started in home

--- controllers/home.js
+++ controllers/home.js
@@ -1,9 +1,32 @@
 const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
+
+//ALL POSTS
 router.get('/', async (req, res) =>{
 
-})
+});
+
+//ONE POST
+router.get('/post/:id', async (req, res) =>{
+
+});
 
 
+//SIGN UP
+router.get('/signup', async (req, res) =>{
+
+});
+
+//LOG IN
+router.get('/login', async (req, res) =>{
+
+});
+
+//LOG OUT 
+router.get('/logout', async (req, res) =>{
+    res.render('logout');
+});
 
 module.exports = router;
