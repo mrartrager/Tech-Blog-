1d147dfce103ad8f7daf28ce5bb19eb114032bcd
Matt Waldron <58498074+mrartrager@users.noreply.github.com>
2022-12-03 16:30:46 -0600

    starting to work on seeds_created SeedsJS

---
+++ controllers/dashboard.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const sequelize = require('../../config/connection')
+// const { User, Post, Comment } = require('../../models/');
+
+
+router.post()
+
+
+module.exports = router
---
+++ seeds/seeds.js
@@ -0,0 +1,12 @@
+const sequelize = require('../config/connection');
+const seedUsers = require('./userSeeds.js');
+const seedComments = require('./commentSeeds.js');
+const seedPosts = require('./postSeeds.js');
+
+const seedData = async () =>{
+
+}
+
+
+
+seedData();
